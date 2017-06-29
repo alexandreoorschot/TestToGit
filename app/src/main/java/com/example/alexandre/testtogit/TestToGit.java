d15a4d70739777d946ba48373661bf07e30cbfb5
alexandreoorschot <alevan3818@gmail.com>
2017-06-28 21:05:37 -0300

    second commit

---
+++ app/src/main/java/com/example/alexandre/testtogit/TestToGit.java
@@ -0,0 +1,13 @@
+package com.example.alexandre.testtogit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestToGit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_to_git);
+    }
+}
